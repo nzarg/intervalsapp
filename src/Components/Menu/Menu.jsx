98d07faed78c3627356f4020303abbc55ea8ce20
nzarg <nzarg@example.com>
2023-03-06 10:12:00 +1300

    stopwatch/countdown

---
+++ src/Components/Menu/Menu.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Menu.css";
+
+export default function Menu(props) {
+const StartButton = (
+	<div className="btn-grp">
+	<div className="btn btn-two"
+		onClick={props.handleStopwatch}>
+		Stopwatch
+	</div>
+	<div className="btn btn-two"
+		onClick={props.handleIntevals}>
+		Intervals
+	</div>
+	</div>
+);
+const ActiveButtons = (
+	<div className="btn-grp">
+    <div className="btn btn-two"
+      onClick={props.handleGoBack}>
+      Go Back
+    </div>
+	</div>
+);
+
+return (
+	<div className="Control-Buttons">
+	<div>{props.goBack ? ActiveButtons : StartButton}</div>
+	</div>
+);
+}
--- src/Components/StopWatch/StopWatch.jsx
+++ src/Components/StopWatch/StopWatch.jsx
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
 import "./StopWatch.css";
 import Timer from "../Timer/Timer";
+import Menu from "../Menu/Menu";
 import ControlButtons from "../ContolButtons/ControlButtons";
 
 function StopWatch() {
 const [isActive, setIsActive] = useState(false);
 const [isPaused, setIsPaused] = useState(true);
 const [time, setTime] = useState(0);
+const [goBack, setGoBack] = useState(false);
+const [countdown, setCountdown] = useState(0);
+const [decrease, setDecrease] = useState(false)
 
 React.useEffect(() => {
 	let interval = null;
 
 	if (isActive && isPaused === false) {
+	let value = decrease? 10 : -10;
 	interval = setInterval(() => {
-		setTime((time) => time + 10);
+		setTime((time) => time + value);
 	}, 10);
 	} else {
 	clearInterval(interval);
@@ -21,7 +26,7 @@ React.useEffect(() => {
 	return () => {
 	clearInterval(interval);
 	};
-}, [isActive, isPaused]);
+}, [isActive, isPaused, decrease]);
 
 const handleStart = () => {
 	setIsActive(true);
@@ -37,9 +42,26 @@ const handleReset = () => {
 	setTime(0);
 };
 
+const handleStopwatch = () => {
+	setDecrease(false);
+	setCountdown(0);
+	setGoBack(true);
+};
+
+const handleIntevals = () => {
+	setDecrease(true);
+	setCountdown(300000)
+	setGoBack(true);
+}
+
+const handleGoBack = () => {
+	setGoBack(false);
+};
+
 return (
 	<div className="stop-watch">
-	<Timer time={time} />
+	<Menu handleStopwatch={handleStopwatch} handleIntevals={handleIntevals} handleGoBack={handleGoBack} goBack={goBack} />
+	<Timer time={time} countdown={countdown}/>
 	<ControlButtons
 		active={isActive}
 		isPaused={isPaused}
